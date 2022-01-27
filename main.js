10c3d78fe1427f957e1d99876213efc61149e658
vishi12 <77247243+vishi12@users.noreply.github.com>
2022-01-27 18:57:36 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent("canvas");
+
+	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent("game_console");
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
